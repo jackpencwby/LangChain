86fc73ac841f558cb35c3ca2ae1990ce607a584d
jackpencwby <kajonyod.jack@gmail.com>
2025-11-10 20:45:17 +0700

    Refactor document loading and splitting to improve data processing

--- rag.py
+++ rag.py
@@ -1,7 +1,10 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = TextLoader("data.txt", encoding="utf-8")
 documents = loader.load()
-print(documents)
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+chunks = text_splitter.split_documents(documents)
+print(chunks)
 
