e34ccc2577e484374073268b2c0b7e4878c2f36d
jackpencwby <kajonyod.jack@gmail.com>
2025-11-10 20:58:11 +0700

    Refactor document processing to include embeddings and vector store integration

--- rag.py
+++ rag.py
@@ -1,10 +1,21 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 loader = TextLoader("data.txt", encoding="utf-8")
 documents = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
 chunks = text_splitter.split_documents(documents)
-print(chunks)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+
+vectorstore = FAISS.from_documents(chunks, embeddings)
+
+retriever = vectorstore.as_retriever()
+
 
