f6839c1f10b47f4597f46ddabec8217db41d9b71
jackpencwby <kajonyod.jack@gmail.com>
2025-11-10 21:07:23 +0700

    Implement RAG chain with prompt and LLM for enhanced question answering

--- rag.py
+++ rag.py
@@ -2,6 +2,10 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,5 +21,24 @@ embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 vectorstore = FAISS.from_documents(chunks, embeddings)
 
 retriever = vectorstore.as_retriever()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "ใช้ข้อมูลจากเอกสารในการตอบคำถามให้เข้าใจได้ง่าย กระชับ เเละชัดเจน"),
+    ("human", "ข้อมูลที่เกี่ยวข้อง: {context}\nคำถาม: {question}")
+])
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+response = rag_chain.invoke("เครื่อง AED คืออะไร")
+print(response)
+
+
 
 
